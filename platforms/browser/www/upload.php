4e2da6bc234d8bffe8800aa820a07004666abeff
Rafael Lima <rafaeloliveiralima@ibest.com.br>
2018-11-30 14:16:54 -0200

    versão com imagens rolando
    versão com imagem rolando

--- platforms/browser/www/upload.php
+++ platforms/browser/www/upload.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 //print_r(json_encode($_FILES));
 //str_replace("world","Peter","Hello world!");
-$arquivo = date('Ymdhis').'.jpg';
+$arquivo = '.jpg';//'date('Ymdhis').'.jpg';
 //echo "<br>";
 //echo date('Y-m-d h:i:s');
 //$new_image_name = str_replace('.jpg','',urldecode($_FILES["file"]["name"])).".jpg";
